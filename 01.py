1b2309c53aa0485aa830482936a53266ca8c85a4
yu-rashia <uajag4y@gmail.com>
2018-12-01 15:20:52 +0900

    login画面実装

--- 01.py
+++ 01.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return '''
+        <form action ="/login" method ="post">
+            Passwaod: <input type="text"><br>
+            <input type="submit">K
+        </form>
+        '''
+    if request.method == "POST":
+        return "Logged in"
+
+    print(request.method)
+    return "Login page"
 
 
 if __name__ == "__main__":
