3901c27f6e6835b45846dbb05ee10d9fd5445697
noopy0415 <noopy0415@gmail.com>
2018-12-01 16:55:11 +0900

    吉と凶のメッセージを追加

--- omikuji.py
+++ omikuji.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 
 @app.route("/omikuji")
 def omikuji():
-    omikuji_list = ["大吉", "吉", "凶"]
+    result = random.choice(["大吉", "吉", "凶"])
 
-    omikujikekka = random.choice(omikuji_list)
-
-    return render_template("omikuji.html", omikujikekka=omikujikekka)
+    return render_template("omikuji.html", result=result)
 
 
 if __name__ == "__main__":
